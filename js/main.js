93290d025a0771e9c9be785aae947cd3ba815592
FespyAR0 <paulo-mega@hotmail.com>
2017-09-10 15:33:29 -0300

    a

--- js/main.js
+++ js/main.js
@@ -1,21 +1,65 @@
-$('.equipe-home .container > :first').addClass('active');
+// $('.equipe-home .container > :first').addClass('active');
 
 
-(function(){
+// (function(){
 
-function iniSlide(){
+// function iniSlide(){
 
-	var activeSlide = $('.slide_item.active'),
-		nextSlide = activeSlide.next();
+// 	var activeSlide = $('.slide_item.active'),
+// 		nextSlide = activeSlide.next();
 
-	if(nextSlide.length == 0){
-		nextSlide = $('.equipe-home .container > :first');
-	}
+// 	if(nextSlide.length == 0){
+// 		nextSlide = $('.equipe-home .container > :first');
+// 	}
 
-	activeSlide.removeClass('active');
-	nextSlide.addClass('active');
-}
+// 	activeSlide.removeClass('active');
+// 	nextSlide.addClass('active');
+// }
 
-setInterval(iniSlide, 6000);
+// setInterval(iniSlide, 6000);
 
-})();
+// })();
+
+
+Visibility.onVisible(function(){
+
+	setInterval(function () {
+		$(".intro-home .titulo-2").addClass("active");
+	}, 800);
+
+	setTimeout(function () {
+		$(".intro-home .align-3").addClass("active");
+	}, 1800);
+
+	setTimeout(function () {
+		$(".intro-home .align-2").addClass("active");
+	}, 1400);
+
+	setInterval(function () {
+		$(".intro-home img").addClass("active");
+	}, 3300);
+
+	setInterval(function () {
+		$(".intro-home img").attr("data-anime", "flutuar");
+	}, 3000);
+});
+
+
+// MENU MOBILE //
+$('.mobile-btn').click(function(){
+	$(this).toggleClass('active'),
+	$('.nav').toggleClass('active');
+});
+
+
+
+
+// MENU DESKTOP //
+$('.pdes-btn').click(function(){
+	$(this).toggleClass('active'),
+	$('.menu-pdes').toggleClass('active'),
+	$('.des-1').toggleClass('active'),
+	$('.des-2').toggleClass('active'),
+	$('.des-3').toggleClass('active'),
+	$('.des-4').toggleClass('active');
+});
