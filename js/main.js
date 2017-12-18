786f84255647e8a90c477e7a449d10e1f1f789e8
FespyAR0 <paulo-mega@hotmail.com>
2017-12-18 20:51:23 -0200

    3

--- js/main.js
+++ js/main.js
@@ -1,4 +1,27 @@
 
+$('.slide > :first').addClass('active');
+
+
+(function(){
+
+function iniSlide(){
+
+	var activeSlide = $('.slide_item.active'),
+		nextSlide = activeSlide.next();
+
+	if(nextSlide.length == 0){
+		nextSlide = $('.slide > :first');
+	}
+
+	activeSlide.removeClass('active');
+	nextSlide.addClass('active');
+}
+
+setInterval(iniSlide, 10000);
+
+})();
+
+
 // MENU MOBILE //
 $('.mobile-btn').click(function(){
 	$(this).toggleClass('active'),
@@ -64,3 +87,8 @@ $(document).ready(function() {
     
  
 });
+
+
+
+
+
